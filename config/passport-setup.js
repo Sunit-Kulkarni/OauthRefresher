5d6d84d132f8d28d5b30c3c08cdec1ffeca3e815
Sunit Kulkarni <sunit.kulkarni@protonmail.com>
2018-07-22 15:18:48 -0400

    added google credential keys

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,11 +1,14 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('../keys/dev');
 
 passport.use(
   new GoogleStrategy({
     //options for the strategy
+    clientID = keys.google.clientID,
+    clientSecret = keys.google.clientSecret
   }),
   () => {
-    //passport callback function
+    //passport callback function 
   }
 );
---
+++ keys/dev.js
@@ -0,0 +1,9 @@
+//add to .gitingore
+
+module.exports = {
+  google: {
+    clientID:
+      '566895909510-fcfgemb61lujdu3ah7u8ardhbls6c01n.apps.googleusercontent.com',
+    clientSecret: '6F9p4vsMgEfMLX1dKhxkTMx6'
+  }
+};
