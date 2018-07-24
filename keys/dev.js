a7e2bee3c15a340fb967f36e5f9b2a3efed685df
Sunit Kulkarni <sunit.kulkarni@protonmail.com>
2018-07-24 16:50:29 -0400

    added mongodb database URI

--- keys/dev.js
+++ keys/dev.js
@@ -5,5 +5,9 @@ module.exports = {
     clientID:
       '566895909510-fcfgemb61lujdu3ah7u8ardhbls6c01n.apps.googleusercontent.com',
     clientSecret: '6F9p4vsMgEfMLX1dKhxkTMx6'
+  },
+  mongodb: {
+    dbURI:
+      'mongodb://sk-google:helloworld1@ds147451.mlab.com:47451/sk-google-oath-test'
   }
 };
