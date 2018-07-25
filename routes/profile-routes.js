bc79d66e04df058bdd36a7aef4a455783e96b480
Sunit Kulkarni <sunit.kulkarni@protonmail.com>
2018-07-24 20:30:31 -0400

    created profile view and authentication middleware to see if user is logged in

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-  res.send('you are logged in. This is your profile: ' + req.user.username);
+//middleware for checking if user is logged in
+const authCheck = (req, res, next) => {
+  if (!req.user) {
+    //if user is not logged in
+    res.redirect('/auth/login');
+  } else {
+    //if logged in
+    next(); //go to next middleware
+  }
+};
+
+router.get('/', authCheck, (req, res) => {
+  res.render('profile', { user: req.user });
 });
 
 module.exports = router;
